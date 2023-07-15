811a52edd4bbf2fd371c85dea593716397e21a5e
bboodd <92453279+bboodd@users.noreply.github.com>
2023-07-16 08:59:09 +0900

    영속성관리

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,35 @@
-package jpa-basic;
+package hellojpa;
 
-public class Main {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            //영속
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+
+            em.flush();
+
+            System.out.println("===================");
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
     }
 }
